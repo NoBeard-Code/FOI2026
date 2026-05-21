92d910b21fe303708fc1de5a1137c0f0614ae0dd
Domagoj <dvlahek23@student.foi.hr>
2026-05-21 19:16:34 +0200

    Delete src/FOI2026.WarehouseFlow.Web directory - ispravak kreiranja projekta s ispravnim nazivima

--- src/FOI2026.WarehouseFlow.Web/FOI2026.WarehouseFlow/Components/Account/PasskeyInputModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FOI2026.WarehouseFlow.Components.Account
-{
-    public class PasskeyInputModel
-    {
-        public string? CredentialJson { get; set; }
-        public string? Error { get; set; }
-    }
-}
--- src/FOI2026.WarehouseFlow.Web/FOI2026.WarehouseFlow/Components/Account/PasskeyOperation.cs
+++
@@ -1,8 +0,0 @@
-namespace FOI2026.WarehouseFlow.Components.Account
-{
-    public enum PasskeyOperation
-    {
-        Create = 0,
-        Request = 1,
-    }
-}
