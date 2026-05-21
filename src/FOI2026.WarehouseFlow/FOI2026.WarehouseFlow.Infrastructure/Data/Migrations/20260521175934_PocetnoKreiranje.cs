edfbfff32e0e76293ed21891821748cda9f79300
dvlahek23 <domag@LAPTOP>
2026-05-21 20:41:35 +0200

    Kreiran .infrastructure projekt i premještan Data folder u njega, dodan connection string

---
+++ src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Infrastructure/Data/Migrations/20260521175934_PocetnoKreiranje.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FOI2026.WarehouseFlow.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class PocetnoKreiranje : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
