d5dd18c06794eb2944033071af3982f158ee7dd2
emucic23 <emucic23@student.foi.hr>
2026-05-22 15:37:25 +0200

    Kreirana klasa StockHistoryEntry unutar servisnih modela. Definisana je struktura podataka za potrebe prikaza povijesti promjena zaliha koja sadrži informacije o datumu promjene, punom imenu korisnika koji je izvršio akciju, vrsti promjene, količini i novom stanju zaliha nakon unesene izmjene. #11

---
+++ src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Services/Models/StockHistoryEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FOI2026.WarehouseFlow.Services.Models
+{
+    public class StockHistoryEntry
+    {
+        public DateTime Date { get; set; }
+        public string UserFullName { get; set; }
+        public string ChangeType { get; set; }
+        public int Quantity { get; set; }
+        public int NewStock { get; set; }
+    }
+}
