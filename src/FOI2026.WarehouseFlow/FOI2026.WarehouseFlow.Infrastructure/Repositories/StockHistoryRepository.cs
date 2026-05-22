84f381392bdd0576de07a63d13fd30bc7be2b160
emucic23 <emucic23@student.foi.hr>
2026-05-22 15:40:50 +0200

    Dodana je konkretna logika koja kroz metode GetOrdersByArticleIdAsync i GetDeliveryNotesByArticleIdAsync asinkrono povlači narudžbe i otpremnice iz baze podataka filtrirane prema identifikatoru artikla, uključujući povezane stavke i podatke o korisnicima koji su kreirali dokumente. #11

---
+++ src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Infrastructure/Repositories/StockHistoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FOI2026.WarehouseFlow.Infrastructure.Data.Models;
+using FOI2026.WarehouseFlow.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace FOI2026.WarehouseFlow.Infrastructure.Repositories
+{
+    public class StockHistoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StockHistoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByArticleIdAsync(int articleId)
+        {
+            return await _context.Orders
+                .Include(o => o.OrderItems)
+                .Include(o => o.User)
+                .Where(o => o.OrderItems.Any(oi => oi.ArticleId == articleId))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<DeliveryNote>> GetDeliveryNotesByArticleIdAsync(int articleId)
+        {
+            return await _context.DeliveryNotes
+                .Include(d => d.DeliveryNoteItems)
+                .Include(d => d.User)
+                .Where(d => d.DeliveryNoteItems.Any(di => di.ArticleId == articleId))
+                .ToListAsync();
+        }
+    }
+}
+
