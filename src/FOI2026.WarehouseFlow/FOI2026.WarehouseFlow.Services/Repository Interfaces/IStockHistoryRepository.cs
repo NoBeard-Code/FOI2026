226a7b5b797b487d68c2a2be0dd02402e4b8a1d4
emucic23 <emucic23@student.foi.hr>
2026-05-22 15:34:18 +0200

    Kreirano sučelje IStockHistoryRepository za povijest promjena zaliha #11

---
+++ src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Services/Repository Interfaces/IStockHistoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FOI2026.WarehouseFlow.Infrastructure.Data.Models;
+
+namespace FOI2026.WarehouseFlow.Services.Repository_Interfaces
+{
+    public interface IStockHistoryRepository
+    {
+        Task<IEnumerable<Order>> GetOrdersByArticleIdAsync(int articleId);
+        Task<IEnumerable<DeliveryNote>> GetDeliveryNotesByArticleIdAsync(int articleId);
+    }
+}
