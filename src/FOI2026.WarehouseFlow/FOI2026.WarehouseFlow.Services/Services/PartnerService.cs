38a346d409720654fa882a300e0683a6126f4ce4
skristo22 <skristo22@student.foi.hr>
2026-05-28 18:44:58 +0200

    Dodana metoda u servis koja omogućuje dodavanje dobavljača. #71

--- src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Services/Services/PartnerService.cs
+++ src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Services/Services/PartnerService.cs
@@ -6,5 +6,22 @@ namespace FOI2026.WarehouseFlow.Services.Services
 {
     public class PartnerService
     {
+        private readonly IPartnerRepository _partnerRepository;
+
+        public PartnerService(IPartnerRepository partnerRepository)
+        {
+            _partnerRepository = partnerRepository;
+        }
+
+        public async Task<IEnumerable<Partner>> GetAllSuppliersAsync()
+        {
+            return await _partnerRepository.GetAllAsync();
+        }
+
+        public async Task AddSupplierAsync(Partner partner)
+        {
+            partner.IsSupplier = true;
+            await _partnerRepository.AddAsync(partner);
+        }
     }
 }
