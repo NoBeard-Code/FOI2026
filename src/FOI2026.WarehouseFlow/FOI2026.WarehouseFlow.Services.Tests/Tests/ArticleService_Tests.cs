c008e0b99c34061494f5d86201ce1aa8c1241176
emucic23 <emucic23@student.foi.hr>
2026-05-28 19:29:00 +0200

    Dodana klasa za testiranje ArticleService servisa.

---
+++ src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Services.Tests/Tests/ArticleService_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FOI2026.WarehouseFlow.Infrastructure.Data.Models;
+using FOI2026.WarehouseFlow.Services.Repository_Interfaces;
+using FOI2026.WarehouseFlow.Services.Services;
+using FakeItEasy;
+
+namespace FOI2026.WarehouseFlow.Services.Tests.Tests
+{
+    public class ArticleService_Tests
+    {
+        private readonly IArticleRepository _articleRepository;
+        private readonly ArticleService _service;
+
+        public ArticleService_Tests()
+        {
+            _articleRepository = A.Fake<IArticleRepository>();
+            _service = new ArticleService(_articleRepository);
+        }
+    }
+}
