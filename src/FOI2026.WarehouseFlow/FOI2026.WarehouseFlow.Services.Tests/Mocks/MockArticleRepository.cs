6579fdc335a91409dc274bccdaac4a66acaca44a
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-05-29 09:09:37 +0200

    mock

---
+++ src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Services.Tests/Mocks/MockArticleRepository.cs
@@ -0,0 +1,39 @@
+﻿using FOI2026.WarehouseFlow.Infrastructure.Data.Models;
+using FOI2026.WarehouseFlow.Services.Repository_Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FOI2026.WarehouseFlow.Services.Tests.Mocks;
+
+internal class MockArticleRepository : IArticleRepository
+{
+    public Task AddAsync(Article article)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<IEnumerable<Article>> GetAllAsync()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<IEnumerable<Article>> GetAllWithStockDataAsync()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<Article> GetByIdAsync(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task UpdateAsync(Article article)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ src/FOI2026.WarehouseFlow/FOI2026.WarehouseFlow.Services.Tests/Tests/ArticleRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using FOI2026.WarehouseFlow.Services.Tests.Mocks;
+using Xunit;
+
+namespace FOI2026.WarehouseFlow.Services.Tests.Tests;
+
+public class ArticleRepositoryTests
+{
+    [Fact]
+    public void ArticleRepository_AddAsync_Init()
+    {
+        // Arrange
+
+        // declare art repo but with injected mock implementation
+
+        var articleRepository = new MockArticleRepository();
+
+
+        // Act
+
+        // Assert
+
+
+    }
+
+
+}
